7582ee981ff3275b8ae68b02ae573ce457a83afa
Danielafsp <135218019+Danielafsp@users.noreply.github.com>
2024-06-04 00:38:21 +0100

    Created Search form & added axios

---
+++ src/Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <form>
+      <input type="search" placeholder="Enter a city..." />
+      <input type="submit" value="Search" />
+    </form>
+  );
+}
